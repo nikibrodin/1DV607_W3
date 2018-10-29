5a2e144d7765a0eda9d61c392fb34d89f93291ee
borkanee <grubesic.boris@gmail.com>
2018-10-29 11:00:51 +0100

    space fix

--- view/IView.cs
+++ view/IView.cs
@@ -16,7 +16,5 @@ namespace BlackJack.view
         void DisplayPlayerHand(IEnumerable<model.Card> a_hand, int a_score);
         void DisplayDealerHand(IEnumerable<model.Card> a_hand, int a_score);
         void DisplayGameOver(bool a_dealerIsWinner);
-
-
     }
 }
